22c3d56ee854eadb0d704ea23aa5b662ce3c9595
elifoksas <62424342+elifoksas@users.noreply.github.com>
2024-04-01 15:38:51 +0300

    TDD

--- app/src/main/java/com/elifoban/androidtesting/Tax.kt
+++ app/src/main/java/com/elifoban/androidtesting/Tax.kt
@@ -2,6 +2,9 @@ package com.elifoban.androidtesting
 
 class Tax {
 
+    //TDD - Test Driven Development
+    // is a software development approach where tests are written before the actual code, guiding the development process and ensuring that the code meets the specified requirements.
+
 
     fun calculateTax(grossIncome: Double, taxRate: Double) : Double{
         return grossIncome * taxRate
